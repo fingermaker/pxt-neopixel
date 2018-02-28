6908f2f0c3162a4636e7496e5736c493e9cc6c1c
fingermaker <xiang.jin@qq.com>
2018-02-28 11:33:30 +0800

    修改笔误
    修改笔误

--- neopixel.ts
+++ neopixel.ts
@@ -192,7 +192,7 @@ namespace neopixel {
         //% blockId="neopixel_set_brightness" block="%strip|set brightness %brightness" blockGap=8
         //% weight=59
         //% parts="neopixel" advanced=true
-        setBrigthness(brightness: number): void {
+        setBrightness(brightness: number): void {
             this.brightness = brightness & 0xff;
         }
 
